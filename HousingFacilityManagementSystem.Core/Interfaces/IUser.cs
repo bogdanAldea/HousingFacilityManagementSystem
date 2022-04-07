88a34ef2edf45cd7a2df0914fa12ef5ecf4cae5e
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-07 09:04:51 +0300

    fixed property typo

--- HousingFacilityManagementSystem.Core/Interfaces/IUser.cs
+++ HousingFacilityManagementSystem.Core/Interfaces/IUser.cs
@@ -9,7 +9,7 @@ namespace Project.Core.Interfaces
     public interface IUser
     {
         public int Id { get; set; }
-        public string FIrstName { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
         public string Username { get; set; }
