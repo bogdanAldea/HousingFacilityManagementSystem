93903fd9b27819d69e30450515b852089c8b68b1
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-07 22:18:21 +0300

    defined factory interface for consumable utilities

---
+++ HousingFacilityManagementSystem.Core/Interfaces/IConsumableUtilityFactory.cs
@@ -0,0 +1,16 @@
+﻿using HousingFacilityManagementSystem.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingFacilityManagementSystem.Core.Factories
+{
+    public interface IConsumableUtilityFactory<IConsumable>
+    {
+
+        public IConsumable CreateConsumableUtility(int indexMeter, Utility utility);
+
+    }
+}
