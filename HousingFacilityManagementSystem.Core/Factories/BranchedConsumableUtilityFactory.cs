e15a8142b50b6390f5d2359d77c501d9cd7fb8df
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-07 22:15:24 +0300

    implemented factories for consumable utilities

---
+++ HousingFacilityManagementSystem.Core/Factories/BranchedConsumableUtilityFactory.cs
@@ -0,0 +1,17 @@
+﻿using HousingFacilityManagementSystem.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingFacilityManagementSystem.Core.Factories
+{
+    public class BranchedConsumableUtilityFactory : IConsumableUtilityFactory<BranchedConsumableUtility>
+    {
+        public BranchedConsumableUtility CreateConsumableUtility(int indexMeter, Utility utility)
+        {
+            return new BranchedConsumableUtility(indexMeter: indexMeter, utility: utility);
+        }
+    }
+}
---
+++ HousingFacilityManagementSystem.Core/Factories/MasterConsumableUtilityFactory.cs
@@ -0,0 +1,17 @@
+﻿using HousingFacilityManagementSystem.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingFacilityManagementSystem.Core.Factories
+{
+    internal class MasterConsumableUtilityFactory : IConsumableUtilityFactory<MasterConsumableUtility>
+    {
+        public MasterConsumableUtility CreateConsumableUtility(string name, int totalIndexMeter, Utility utility)
+        {
+            return new MasterConsumableUtility()
+        }
+    }
+}
