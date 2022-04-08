5384bfcb9dd75472e6f76c26831e824c3173e7b3
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-08 17:35:42 +0300

    ...

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,33 +1,12 @@
 ﻿using HousingFacilityManagementSystem.Core.Enums;
-using HousingFacilityManagementSystem.Core.Factories;
 using HousingFacilityManagementSystem.Core.Interfaces;
 using HousingFacilityManagementSystem.Core.Models;
+using HousingFacilityManagementSystem.Core.Models.Utilities;
+using HousingFacilityManagementSystem.Core.Repositories;
 using HousingFacilityManagementSystem.Infrastructure;
 
-List<Utility> coreUtilities = new List<Utility>() 
-{ 
-    new Utility(name: "Cold Water", utilityType: UtilityType.Consumable),
-    new Utility(name: "Hot Water", utilityType: UtilityType.Consumable),
-    new Utility(name: "Gas", utilityType: UtilityType.Consumable),
-    new Utility(name: "Heat", utilityType: UtilityType.Consumable),
-    new Utility(name: "Electricity", utilityType: UtilityType.Consumable),
-};
 
-List<MasterConsumableUtility> masterUtilities = new List<MasterConsumableUtility>()
-{
-    new MasterConsumableUtility(utility: coreUtilities[0], indexMeter: 54686453),
-    new MasterConsumableUtility(utility: coreUtilities[1], indexMeter: 84653212),
-    new MasterConsumableUtility(utility: coreUtilities[2], indexMeter: 48970134),
-    new MasterConsumableUtility(utility: coreUtilities[3], indexMeter: 36520124),
-};
+UniversalUtility util = new UniversalUtility("Water", UtilityType.Consumable, 5.50m);
 
-InMemoryMasterConsumableUtilityRepository masterUtilityRepo = new InMemoryMasterConsumableUtilityRepository(masterUtilities);
-
-var x = new MasterConsumableUtility(utility: coreUtilities[4], indexMeter: 54682312);
-masterUtilityRepo.Add(x);
-
-
-foreach (var master in masterUtilityRepo.GetAll())
-{
-    Console.WriteLine(master);
-}
+var x = util.GetType().GetProperty("Price").GetValue(util);
+Console.WriteLine(x);
