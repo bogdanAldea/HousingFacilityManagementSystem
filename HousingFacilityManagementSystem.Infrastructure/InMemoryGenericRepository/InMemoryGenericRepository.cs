b9ab1d91e0531308f2b0a98b519cc6b9ba2f01dd
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-11 10:07:47 +0300

    added generic repository to its own directory

---
+++ HousingFacilityManagementSystem.Infrastructure/InMemoryGenericRepository/InMemoryGenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using HousingFacilityManagementSystem.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingFacilityManagementSystem.Core.Repositories
+{
+    public class InMemoryGenericRepository<T> : IGenericRepository<T> where T : class, IEntity
+    {
+
+        private List<T> _inMemoryContext;
+
+        public InMemoryGenericRepository(List<T> inMemoryContext)
+        {
+            _inMemoryContext = inMemoryContext;
+        }
+
+        public void Add(T entity)
+        {
+            if (entity == null) 
+            { 
+                throw new ArgumentNullException(); 
+            }
+            _inMemoryContext.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            if (_inMemoryContext.Count() < 1) 
+            { 
+                throw new InvalidOperationException(); 
+            }
+            _inMemoryContext.Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _inMemoryContext;
+        }
+
+        public T GetById(int id)
+        {
+            return _inMemoryContext.FirstOrDefault(entity => entity.Id == id);
+        }
+
+        public void Update(T entity)
+        {
+            IEntity oldEntity = _inMemoryContext.Where(oldEntity => oldEntity.Id == entity.Id).First();
+            oldEntity = entity;
+
+        }
+    }
+}
