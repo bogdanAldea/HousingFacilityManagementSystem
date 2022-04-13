dba0cf388c0cebe6b09e8153c052f2a039aa3a42
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-13 05:05:48 +0300

    implemented repository that executes operations against a database

---
+++ HousingFacilityManagementSystem.Infrastructure/GenericRepositories/EFCoreRepository.cs
@@ -0,0 +1,47 @@
+﻿using HousingFacilityManagementSystem.Core.Interfaces;
+using HousingFacilityManagementSystem.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingFacilityManagementSystem.Infrastructure.GenericRepositories
+{
+    public class EFCoreRepository<T> : IGenericRepository<T> where T : class, IEntity
+    {
+
+        private readonly DbContext _housingFacilityRepository;
+
+        public EFCoreRepository(DbContext houdingFacilityRepository)
+        {
+            _housingFacilityRepository = houdingFacilityRepository;
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
