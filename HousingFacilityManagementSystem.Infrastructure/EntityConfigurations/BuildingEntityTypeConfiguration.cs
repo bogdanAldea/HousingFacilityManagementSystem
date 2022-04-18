e9479cf4e7b4097cb563861bdb22785708978b70
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-18 16:39:42 +0300

    entity configurations

---
+++ HousingFacilityManagementSystem.Infrastructure/EntityConfigurations/BuildingEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using HousingFacilityManagementSystem.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingFacilityManagementSystem.Infrastructure.EntityConfigurations
+{
+    internal class BuildingEntityTypeConfiguration : IEntityTypeConfiguration<Building>
+    {
+        public void Configure(EntityTypeBuilder<Building> builder)
+        {
+            builder
+                .HasOne(building => building.Administrator)
+                .WithOne(building => building.Building)
+                .HasForeignKey<Building>(building => building.AdministratorId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
---
+++ HousingFacilityManagementSystem.Infrastructure/Migrations/20220415123801_DemodbInitialMigration.cs
@@ -0,0 +1,295 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HousingFacilityManagementSystem.Infrastructure.Migrations
+{
+    public partial class DemodbInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Administrators",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Administrators", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tenants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tenants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Buildings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdministratorId = table.Column<int>(type: "int", nullable: true),
+                    Capacity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Buildings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Buildings_Administrators_AdministratorId",
+                        column: x => x.AdministratorId,
+                        principalTable: "Administrators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Apartments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenantId = table.Column<int>(type: "int", nullable: true),
+                    NumberInBuilding = table.Column<int>(type: "int", nullable: false),
+                    Residents = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    SurfaceArea = table.Column<double>(type: "float", nullable: false, defaultValue: 0.0),
+                    PaymentDebt = table.Column<double>(type: "float", nullable: false, defaultValue: 0.0),
+                    BuildingId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Apartments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Apartments_Buildings_BuildingId",
+                        column: x => x.BuildingId,
+                        principalTable: "Buildings",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Apartments_Tenants_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "Tenants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MasterConsumableUtilities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    IndexMeter = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    CurrentMonthIndex = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false, defaultValue: 0.0m),
+                    BuildingId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MasterConsumableUtilities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MasterConsumableUtilities_Buildings_BuildingId",
+                        column: x => x.BuildingId,
+                        principalTable: "Buildings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UniversalUtilities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false, defaultValue: 0.0m),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    BuildingId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UniversalUtilities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UniversalUtilities_Buildings_BuildingId",
+                        column: x => x.BuildingId,
+                        principalTable: "Buildings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BranchedConsumableUtilities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AmountToPay = table.Column<decimal>(type: "decimal(18,2)", nullable: false, defaultValue: 0m),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    IndexMeter = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    CurrentMonthIndex = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    IsBranched = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    ApartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BranchedConsumableUtilities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BranchedConsumableUtilities_Apartments_ApartmentId",
+                        column: x => x.ApartmentId,
+                        principalTable: "Apartments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<int>(type: "int", nullable: false),
+                    IssueDate = table.Column<int>(type: "int", nullable: false),
+                    Payment = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Penalties = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsPaid = table.Column<bool>(type: "bit", nullable: false),
+                    ApartmentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Invoices_Apartments_ApartmentId",
+                        column: x => x.ApartmentId,
+                        principalTable: "Apartments",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Administrators_EmailAddress",
+                table: "Administrators",
+                column: "EmailAddress",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Administrators_Username",
+                table: "Administrators",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apartments_BuildingId",
+                table: "Apartments",
+                column: "BuildingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apartments_TenantId",
+                table: "Apartments",
+                column: "TenantId",
+                unique: true,
+                filter: "[TenantId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BranchedConsumableUtilities_ApartmentId",
+                table: "BranchedConsumableUtilities",
+                column: "ApartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BranchedConsumableUtilities_Name",
+                table: "BranchedConsumableUtilities",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Buildings_AdministratorId",
+                table: "Buildings",
+                column: "AdministratorId",
+                unique: true,
+                filter: "[AdministratorId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_ApartmentId",
+                table: "Invoices",
+                column: "ApartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MasterConsumableUtilities_BuildingId",
+                table: "MasterConsumableUtilities",
+                column: "BuildingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MasterConsumableUtilities_Name",
+                table: "MasterConsumableUtilities",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tenants_EmailAddress",
+                table: "Tenants",
+                column: "EmailAddress",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tenants_Username",
+                table: "Tenants",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UniversalUtilities_BuildingId",
+                table: "UniversalUtilities",
+                column: "BuildingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UniversalUtilities_Name",
+                table: "UniversalUtilities",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BranchedConsumableUtilities");
+
+            migrationBuilder.DropTable(
+                name: "Invoices");
+
+            migrationBuilder.DropTable(
+                name: "MasterConsumableUtilities");
+
+            migrationBuilder.DropTable(
+                name: "UniversalUtilities");
+
+            migrationBuilder.DropTable(
+                name: "Apartments");
+
+            migrationBuilder.DropTable(
+                name: "Buildings");
+
+            migrationBuilder.DropTable(
+                name: "Tenants");
+
+            migrationBuilder.DropTable(
+                name: "Administrators");
+        }
+    }
+}
