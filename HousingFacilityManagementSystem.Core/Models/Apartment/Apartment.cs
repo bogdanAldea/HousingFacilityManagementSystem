02ad6baa7a615ebeece202dda74082edd2372b1e
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-19 18:10:47 +0300

    removed unneccessary imports

--- HousingFacilityManagementSystem.Core/Models/Apartment/Apartment.cs
+++ HousingFacilityManagementSystem.Core/Models/Apartment/Apartment.cs
@@ -1,5 +1,5 @@
 ﻿using HousingFacilityManagementSystem.Core.Interfaces;
-using HousingFacilityManagementSystem.Core.Models.Utilities;
+using HousingFacilityManagementSystem.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
