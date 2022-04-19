52be1fd86a0c18421cd83015f13583c412eb6b71
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-19 11:14:06 +0300

    written a test for Building method that adds apartments

---
+++ HousingFacilityManagementSystem.Core.Tests/BuildingTests.cs
@@ -0,0 +1,43 @@
+﻿using HousingFacilityManagementSystem.Core.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingFacilityManagementSystem.Core.Tests
+{
+    [TestFixture]
+    public class BuildingTests
+    {
+
+        private Building _building;
+
+        [SetUp]
+        public void Setup()
+        {
+            _building = new Building(5);
+            _buildingWithApartment = new Building(5);
+        }
+
+        [TestCase(5, 5)]
+        [TestCase(4, 4)]
+        [TestCase(6, 6)]
+        [TestCase(7, 7)]
+        [TestCase(8, 8)]
+        [TestCase(9, 9)]
+        public void CreateApartmentsShouldCreateApartmentsBasedOnBuildingCapacityProperty(int capacity, int expectedLength)
+        {
+            // Arrange
+            _building.Capacity = capacity;
+
+            // Act
+            _building.CreateApartments();
+
+            // Assert
+            Assert.AreEqual(expectedLength, _building.Apartments.Count);
+        }
+
+    }
+}
