cd0bba42e5b7852f0badf62719f7f9ba33d28c05
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-19 18:07:39 +0300

    added API project

---
+++ HousingFacilityManagementSystem.Api/Controllers/BuildingsController.cs
@@ -0,0 +1,38 @@
+﻿using HousingFacilityManagementSystem.Application.Buildings.Commands;
+using HousingFacilityManagementSystem.Application.Buildings.Queries;
+using HousingFacilityManagementSystem.Core.Models;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousingFacilityManagementSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuildingsController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public BuildingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBuildingById(int id)
+        {
+            var command = new GetBuildingByIdQuery(id);
+            var building = _mediator.Send(command);
+            return Ok(building);    
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBuilding(int capacity)
+        {
+            var command = new CreateBuildingCommand(capacity);
+            var building = _mediator.Send(command);
+            return Ok(building);
+        }
+    }
+}
---
+++ HousingFacilityManagementSystem.Api/Program.cs
@@ -0,0 +1,51 @@
+using HousingFacilityManagementSystem.Application.Buildings.Commands;
+using HousingFacilityManagementSystem.Core.Models;
+using HousingFacilityManagementSystem.Core.Repositories;
+using HousingFacilityManagementSystem.Infrastructure.Context;
+using HousingFacilityManagementSystem.Infrastructure.GenericRepositories;
+using HousingFacilityManagementSystem.Infrastructure.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add MediatR
+builder.Services.AddMediatR(typeof(CreateBuildingCommand));
+
+
+// Scoped repositories
+
+builder.Services.AddScoped<IRepository<Building>, BuildingRepository>();
+builder.Services.AddScoped<IRepository<Apartment>, ApartmentRepository>();
+builder.Services.AddScoped<IRepository<BranchedConsumableUtility>, BranchedUtilityRepository>();
+builder.Services.AddScoped<IRepository<Invoice>, InvoiceRepository>();
+builder.Services.AddScoped<IRepository<MasterConsumableUtility>, MasterUtilityRepository>();
+
+// Add DbContext
+builder.Services.AddDbContext<HousingFacilityContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("Demo"))
+    );
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
