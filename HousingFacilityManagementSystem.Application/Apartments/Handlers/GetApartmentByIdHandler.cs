8580d66d0aea7509f35fcc245d7ae200934124fc
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-20 16:24:42 +0300

    created command and query for retrieving apartment by id

---
+++ HousingFacilityManagementSystem.Application/Apartments/Handlers/GetApartmentByIdHandler.cs
@@ -0,0 +1,29 @@
+﻿using HousingFacilityManagementSystem.Application.Apartments.Queries;
+using HousingFacilityManagementSystem.Core.Models;
+using HousingFacilityManagementSystem.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingFacilityManagementSystem.Application.Apartments.Handlers
+{
+    public class GetApartmentByIdHandler : IRequestHandler<GetApartmentByIdQuery, Apartment>
+    {
+
+        private readonly IRepository<Apartment> _repository;
+
+        public GetApartmentByIdHandler(IRepository<Apartment> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Apartment> Handle(GetApartmentByIdQuery request, CancellationToken cancellationToken)
+        {
+            Apartment apartment = _repository.GetById(request.Id);
+            return apartment;
+        }
+    }
+}
---
+++ HousingFacilityManagementSystem.Application/Apartments/Queries/GetApartmentByIdQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HousingFacilityManagementSystem.Core.Models;
+
+
+namespace HousingFacilityManagementSystem.Application.Apartments.Queries
+{
+    public class GetApartmentByIdQuery : IRequest<Apartment>
+    {
+        public int Id { get; set; }
+
+        public GetApartmentByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
