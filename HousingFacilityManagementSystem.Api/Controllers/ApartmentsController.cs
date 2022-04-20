6830bff2c43969eb61819cc24637951299c3931d
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-20 16:24:15 +0300

    created controller for apartment endpoint

---
+++ HousingFacilityManagementSystem.Api/Controllers/ApartmentsController.cs
@@ -0,0 +1,29 @@
+﻿using HousingFacilityManagementSystem.Application.Apartments.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousingFacilityManagementSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApartmentsController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public ApartmentsController(IMediator mediator)
+        {
+            _mediator = mediator;   
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetApartmentById(int id)
+        {
+            var command = new GetApartmentByIdQuery(id);
+            var apartment = _mediator.Send(command);
+            return Ok(apartment);
+        }
+
+    }
+}
