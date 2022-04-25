d1622d3901afd6c67379050f8a677a567980edbf
Bogdan <bogdan.aldea0623@gmail.com>
2022-04-25 18:02:39 +0300

    new migrations applied

---
+++ HousingFacilityManagementSystem.Infrastructure/Migrations/20220420143616_OnDeleteBehaviorConfigMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HousingFacilityManagementSystem.Infrastructure.Migrations
+{
+    public partial class OnDeleteBehaviorConfigMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Apartments_Buildings_BuildingId",
+                table: "Apartments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Apartments_Buildings_BuildingId",
+                table: "Apartments",
+                column: "BuildingId",
+                principalTable: "Buildings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Apartments_Buildings_BuildingId",
+                table: "Apartments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Apartments_Buildings_BuildingId",
+                table: "Apartments",
+                column: "BuildingId",
+                principalTable: "Buildings",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ HousingFacilityManagementSystem.Infrastructure/Migrations/20220425144354_MasterUtilBuildingRelationshipConfigFixMigration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HousingFacilityManagementSystem.Infrastructure.Migrations
+{
+    public partial class MasterUtilBuildingRelationshipConfigFixMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MasterConsumableUtilities_Buildings_BuildingId1",
+                table: "MasterConsumableUtilities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MasterConsumableUtilities_BuildingId1",
+                table: "MasterConsumableUtilities");
+
+            migrationBuilder.DropColumn(
+                name: "BuildingId1",
+                table: "MasterConsumableUtilities");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BuildingId1",
+                table: "MasterConsumableUtilities",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MasterConsumableUtilities_BuildingId1",
+                table: "MasterConsumableUtilities",
+                column: "BuildingId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MasterConsumableUtilities_Buildings_BuildingId1",
+                table: "MasterConsumableUtilities",
+                column: "BuildingId1",
+                principalTable: "Buildings",
+                principalColumn: "Id");
+        }
+    }
+}
