48e98457bf1dd8eaafb95c854dff7c6cc907fba8
Bogdan <bogdan.aldea0623@gmail.com>
2022-05-13 16:14:31 +0300

    added repositories for admin and tenant user profiles

---
+++ HousingFacilityManagementSystem.Infrastructure/Repositories/AdminProfileRepository.cs
@@ -0,0 +1,64 @@
+﻿using HousingFacilityManagementSystem.Core.Models.Users;
+using HousingFacilityManagementSystem.Core.Repositories;
+using HousingFacilityManagementSystem.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingFacilityManagementSystem.Infrastructure.Repositories
+{
+    public class AdminProfileRepository : IRepository<AdministratorProfile>
+    {
+
+        private readonly HousingFacilityContext _context;
+
+        public AdminProfileRepository(HousingFacilityContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(AdministratorProfile entity)
+        {
+            _context.Administrators.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(AdministratorProfile entity)
+        {
+            _context.Administrators.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public ICollection<AdministratorProfile> GetAll()
+        {
+            return _context.Administrators
+                           .ToList();
+        }
+
+        public AdministratorProfile GetById(int id)
+        {
+            return _context.Administrators
+                .SingleOrDefault(admin => admin.Id == id);
+        }
+
+        public AdministratorProfile GetByIdentityId(string id)
+        {
+            return _context.Administrators
+                .SingleOrDefault(admin => admin.IdentityId == id);
+        }
+
+        public void Update(AdministratorProfile entity)
+        {
+            AdministratorProfile? existingAdminProfile = _context.Administrators
+                .SingleOrDefault(admin => admin.Id == entity.Id);
+            
+            if (existingAdminProfile != null)
+            {
+                _context.Entry(existingAdminProfile).CurrentValues.SetValues(entity);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ HousingFacilityManagementSystem.Infrastructure/Repositories/TenantProfileRepository.cs
@@ -0,0 +1,59 @@
+﻿using HousingFacilityManagementSystem.Core.Models.Users;
+using HousingFacilityManagementSystem.Core.Repositories;
+using HousingFacilityManagementSystem.Infrastructure.Context;
+
+namespace HousingFacilityManagementSystem.Infrastructure.Repositories
+{
+    public class TenantProfileRepository : IRepository<TenantProfile>
+    {
+
+        private readonly HousingFacilityContext _context;
+
+        public TenantProfileRepository(HousingFacilityContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(TenantProfile entity)
+        {
+            _context.Tenants.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(TenantProfile entity)
+        {
+            _context.Tenants.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public ICollection<TenantProfile> GetAll()
+        {
+            return _context.Tenants
+                .ToList();
+        }
+
+        public TenantProfile GetById(int id)
+        {
+            return _context.Tenants
+                .SingleOrDefault(tenant => tenant.Id == id);
+        }
+
+        public TenantProfile GetByIdentityId(string id)
+        {
+            return _context.Tenants
+                .SingleOrDefault(admin => admin.IdentityId == id);
+        }
+
+        public void Update(TenantProfile entity)
+        {
+            TenantProfile? existingTenantProfile = _context.Tenants
+                .SingleOrDefault(tenant => tenant.Id == entity.Id);
+
+            if (existingTenantProfile != null)
+            {
+                _context.Entry(existingTenantProfile).CurrentValues.SetValues(entity);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
