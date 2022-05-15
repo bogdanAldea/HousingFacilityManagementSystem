c076494b024a674ce516872ee5d2c07939a19701
Bogdan <bogdan.aldea0623@gmail.com>
2022-05-15 18:20:33 +0300

    added identity endpoint controller - empty

---
+++ HousingFacilityManagementSystem.Api/Controllers/IdentityController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousingFacilityManagementSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+    }
+}
