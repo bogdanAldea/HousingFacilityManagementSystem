5fef1dc6ad76487f2523bf53be2d0b4cb3d5327e
Bogdan <bogdan.aldea0623@gmail.com>
2022-05-16 15:16:24 +0300

    implemented auth controller

--- HousingFacilityManagementSystem.Api/Controllers/IdentityController.cs
+++ HousingFacilityManagementSystem.Api/Controllers/IdentityController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using HousingFacilityManagementSystem.Api.DTOs;
+using HousingFacilityManagementSystem.Application.Identity.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HousingFacilityManagementSystem.Api.Controllers
@@ -7,5 +11,32 @@ namespace HousingFacilityManagementSystem.Api.Controllers
     [ApiController]
     public class IdentityController : ControllerBase
     {
+
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public IdentityController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register(UserRegistrationDto userRegistrationDto)
+        {
+            var command = _mapper.Map<RegisterAdminCommand>(userRegistrationDto);
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+        {
+            var command = _mapper.Map<LoginAdminCommand>(userLoginDto);
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
     }
 }
